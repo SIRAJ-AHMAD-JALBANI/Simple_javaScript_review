477882adf8999221091e66fab9b3270a8f1fd9ac
SIRAJ-AHMAD-JALBANI <s4495942@gmail.com>
2025-10-06 10:23:22 +0500

    largest number finder in javaScript

---
+++ largest_number_Finder.js
@@ -0,0 +1,8 @@
+function largestOfAll(arr){
+  let largArr = []
+  for(let value of arr){
+    largArr.push(Math.max(...value))
+  }
+  return largArr;
+}
+console.log(largestOfAll([[13, 27, 18, 26], [4, 5, 1, 3], [32, 35, 37, 39], [1000, 1001, 857, 1]]));
